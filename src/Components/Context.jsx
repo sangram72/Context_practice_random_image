cb13f3f08bd18dff4b22ef8be347fcf33fc9c1bb
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2025-04-26 13:38:52 +0530

    Update Context.jsx

--- src/Components/Context.jsx
+++ src/Components/Context.jsx
@@ -5,8 +5,9 @@ export const choosecolor = createContext()
 
 export const ContextColor  =(props)=>{
     const[colors,setcolors]=useState("red")
+    const [conimage,setconimage]=useState(null)
 return(
-<choosecolor.Provider value={{colors,setcolors}}>
+<choosecolor.Provider value={{colors,setcolors,conimage,setconimage}}>
 {props.children}
 </choosecolor.Provider>
 )
