97f9d3bf63ec29c33c3229a58adde7def4ecab61
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2025-04-26 13:38:31 +0530

    Update Body.jsx

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,60 +1,70 @@
-import React, { useContext, useEffect, useState } from "react"
-import Head from "../Head/Head"
-import Tail from "../Tail/Tail"
-import { choosecolor } from "../Context"
-import load from "../../assets/load.gif"
-const Body =()=>{
-    const { colors } = useContext(choosecolor);
-    const [loading,setloading]=useState(true)
-    const [image,setimage]=useState()
-const fetchdetails=async()=>{
-const baseurl = "https://dog.ceo/api/breeds/image/random"
+import React, { useContext, useEffect, useState } from "react";
+import Head from "../Head/Head";
+import Tail from "../Tail/Tail";
+import { choosecolor } from "../Context";
+import load from "../../assets/load.gif";
 
-try {
-    let response = await fetch(baseurl)
-    let res = await response.json()
-console.log(res)
-setimage(res)
-if(res.message){
-setloading(false)
-}
+const Body = () => {
+  const { colors, conimage, setconimage } = useContext(choosecolor);
+  const [loading, setloading] = useState(true);
 
+  const fetchdetails = async () => {
+    const baseurl = "https://dog.ceo/api/breeds/image/random";
 
-} catch (error) {
-    console.log("error is",error)
-    setloading(true)
-}
-}
+    try {
+      let response = await fetch(baseurl);
+      let res = await response.json();
+      console.log(res);
+      setconimage(res.message);
 
-useEffect(()=>{
-    fetchdetails()
-},[])
-
-    return (
-        <><Head />
-        <br/>
-        <br/>
-        <div style={{ height: "500px", backgroundColor: colors,  display: "flex", 
-  justifyContent: "center", 
-  alignItems: "center" }}>
-   {loading==false? image&&
-<img src={image.message} alt="" style={{height:"80%",width:"50%"}} />
-    
-
-:<img src={load} style={{height:"50%",width:'50%'}}/>}
+      setloading(false); // Set loading to false once the image is fetched
+    } catch (error) {
+      console.log("Error:", error);
+      setloading(true); 
+    }
 
 
-        </div>
-        <button onClick={() => { setloading(true); fetchdetails(); }}>
+  };
+
+  useEffect(() => {
+    if(conimage==null){
+    fetchdetails();
+    }else{
+        setloading(false)
+    }
+  }, []);
+
+  return (
+    <>
+      <Head />
+      <br />
+      <br />
+      <div
+        style={{
+          height: "500px",
+          backgroundColor: colors,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {loading ? (
+          <img src={load} alt="Loading" style={{ height: "50%", width: "50%" }} />
+        ) : (
+          conimage && (
+            <img src={conimage} alt="Random Dog" style={{ height: "80%", width: "50%" }} />
+          )
+        )}
+      </div>
+
+      <button onClick={() => { setloading(true); fetchdetails(); }}>
         Generate Random Dog Image
       </button>
-        <br/>
-        <br/>
-        <Tail/>
-        </>
-   
-    )
-}
-export default Body
+      <br />
+      <br />
+      <Tail />
+    </>
+  );
+};
 
-
+export default Body;
